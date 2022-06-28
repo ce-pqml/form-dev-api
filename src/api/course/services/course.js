07174e0c5f2a1630341255078e96da928b4d7903
ce-pqml <charleselie.piquemal@gmail.com>
2022-06-28 17:53:05 +0200

    test

--- src/api/course/services/course.js
+++ src/api/course/services/course.js
@@ -6,21 +6,4 @@
 
 const { createCoreService } = require("@strapi/strapi").factories;
 
-module.exports = createCoreService("api::course.course", ({ strapi }) => ({
-  async findOne(ctx, params) {
-    return strapi.query("api::course.course").findOne({
-      where: {
-        id: ctx,
-      },
-      ...params,
-      populate: {
-        signatures: {
-          populate: {
-            attendee: true,
-          },
-        },
-        speakers: true,
-      },
-    });
-  },
-}));
+module.exports = createCoreService("api::course.course");
