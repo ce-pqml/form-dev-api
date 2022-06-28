89a0c1b750d44d339fa0b7302ea051d0321cc0cf
ce-pqml <charleselie.piquemal@gmail.com>
2022-06-28 17:56:01 +0200

    refix

--- src/api/course/services/course.js
+++ src/api/course/services/course.js
@@ -6,4 +6,21 @@
 
 const { createCoreService } = require("@strapi/strapi").factories;
 
-module.exports = createCoreService("api::course.course");
+module.exports = createCoreService("api::course.course", ({ strapi }) => ({
+  async findOne(ctx, params) {
+    return strapi.query("api::course.course").findOne({
+      where: {
+        id: ctx,
+      },
+      ...params,
+      populate: {
+        signatures: {
+          populate: {
+            attendee: true,
+          },
+        },
+        speakers: true,
+      },
+    });
+  },
+}));
