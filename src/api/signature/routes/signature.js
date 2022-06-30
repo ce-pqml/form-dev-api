6628f61026b355d0a901aacde0beb5d903a4fd3c
ce-pqml <charleselie.piquemal@gmail.com>
2022-06-30 14:16:03 +0200

    fix route email

--- src/api/signature/routes/signature.js
+++ src/api/signature/routes/signature.js
@@ -24,7 +24,7 @@ const customRouter = (innerRouter, extraRoutes = []) => {
 const myExtraRoutes = [
   {
     method: "GET",
-    path: "/:id/email",
+    path: "/signature/:id/email",
     handler: "api::signature.signature.sendMail",
   },
 ];
