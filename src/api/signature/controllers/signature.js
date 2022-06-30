5abd6147d3fc2d58d61755e06f929209d05f46da
ce-pqml <charleselie.piquemal@gmail.com>
2022-06-30 14:11:11 +0200

    signature email test

--- src/api/signature/controllers/signature.js
+++ src/api/signature/controllers/signature.js
@@ -1,9 +1,28 @@
-'use strict';
+"use strict";
 
 /**
  *  signature controller
  */
 
-const { createCoreController } = require('@strapi/strapi').factories;
+const { createCoreController } = require("@strapi/strapi").factories;
 
-module.exports = createCoreController('api::signature.signature');
+module.exports = createCoreController(
+  "api::signature.signature",
+  ({ strapi }) => ({
+    async sendMail(ctx) {
+      console.log("sendMail");
+      // const { id } = ctx.params;
+      // const entries = await strapi.db.query("api::hello.hello").findMany();
+      await strapi.plugins["email"].services.email.send({
+        to: "charleselie.piquemal@gmail.com",
+        from: "test@strapi.io",
+        replyTo: "test@strapi.io",
+        subject: "Use strapi email provider successfully",
+        html: "Hello world!",
+      });
+
+      console.log("end");
+      ctx.body = { msg: "ok" };
+    },
+  })
+);
--- src/api/signature/routes/signature.js
+++ src/api/signature/routes/signature.js
@@ -1,9 +1,34 @@
-'use strict';
+"use strict";
 
 /**
  * signature router.
  */
 
-const { createCoreRouter } = require('@strapi/strapi').factories;
+const { createCoreRouter } = require("@strapi/strapi").factories;
 
-module.exports = createCoreRouter('api::signature.signature');
+const defaultRouter = createCoreRouter("api::signature.signature");
+
+const customRouter = (innerRouter, extraRoutes = []) => {
+  let routes;
+  return {
+    get prefix() {
+      return innerRouter.prefix;
+    },
+    get routes() {
+      if (!routes) routes = innerRouter.routes.concat(extraRoutes);
+      return routes;
+    },
+  };
+};
+
+const myExtraRoutes = [
+  {
+    method: "GET",
+    path: "/:id/email",
+    handler: "api::signature.signature.sendMail",
+  },
+];
+
+module.exports = customRouter(defaultRouter, myExtraRoutes);
+
+// module.exports = createCoreRouter("api::signature.signature");
