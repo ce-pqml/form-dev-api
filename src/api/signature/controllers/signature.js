96e8438e05f7eeea94b605fb157c13cad733997b
ce-pqml <charleselie.piquemal@gmail.com>
2022-06-30 17:55:52 +0200

    send signature

--- src/api/signature/controllers/signature.js
+++ src/api/signature/controllers/signature.js
@@ -1,27 +1,67 @@
+// @ts-nocheck
 "use strict";
 
 /**
  *  signature controller
  */
-
+const fs = require("fs");
+const path = require("path");
 const { createCoreController } = require("@strapi/strapi").factories;
 
 module.exports = createCoreController(
   "api::signature.signature",
   ({ strapi }) => ({
     async sendMail(ctx) {
-      console.log("sendMail");
-      // const { id } = ctx.params;
-      // const entries = await strapi.db.query("api::hello.hello").findMany();
-      await strapi.plugins["email"].services.email.send({
-        to: "charleselie.piquemal@gmail.com",
-        from: "test@strapi.io",
-        replyTo: "test@strapi.io",
-        subject: "Use strapi email provider successfully",
-        html: "Hello world!",
-      });
+      const { id } = ctx.params;
+      const result = await strapi
+        .service("api::signature.signature")
+        .findOne(id, {
+          populate: {
+            attendee: true,
+            course: true,
+          },
+        });
 
-      console.log("end");
+      const signature = result;
+      const attendee = signature.attendee;
+      const course = signature.course;
+
+      const emailTemplate = {
+        subject: "Demande de signature - <%= course.title %>",
+        text: "Test",
+        html: fs.readFileSync(
+          path.join(
+            __dirname,
+            "..",
+            "..",
+            "..",
+            "email",
+            "signature_template.html"
+          ),
+          "utf8"
+        ),
+      };
+
+      await strapi.plugins["email"].services.email.sendTemplatedEmail(
+        {
+          to: process.env.SMTP_DEFAULT_MAIL,
+        },
+        emailTemplate,
+        {
+          course,
+          attendee,
+          signature,
+        }
+      );
+
+      // await strapi.plugins["email"].services.email.send({
+      //   to: env("SMTP_DEFAULT_MAIL"),
+      //   from: env("SMTP_DEFAULT_MAIL"),
+      //   replyTo: env("SMTP_DEFAULT_MAIL"),
+      //   subject: "Demande de signature - " + course.title,
+      //   html: "Hello world!",
+      // });
+
       ctx.body = { msg: "ok" };
     },
   })
